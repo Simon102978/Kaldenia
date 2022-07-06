2f545357e4576b82e03b58dd6321bf4a3414ca2c
Simon <mitaniel@hotmail.com>
2022-07-06 19:55:56 -0400

    Fe Control - Montre le nombre de FE par personnage

---
+++ Scripts/Custom/Commande/fecontrol.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Server;
+using Server.Mobiles;
+using Server.Targeting;
+using Server.Commands;
+using Server.Gumps;
+
+namespace Server.Scripts.Commands
+{
+    public class fecontrol
+    {
+        public static void Initialize()
+        {
+            CommandSystem.Register("fecontrol", AccessLevel.GameMaster, new CommandEventHandler(fecontrol_OnCommand));
+        }
+
+        public static void fecontrol_OnCommand(CommandEventArgs e)
+        {
+			Mobile from = e.Mobile;
+
+			if (from is CustomPlayerMobile cp)
+				from.SendGump(new FeControlGump(cp,new System.Collections.Generic.Dictionary<CustomPlayerMobile, int>(), 0));
+
+
+
+        }
+
+
+    }
+}
---
+++ Scripts/Custom/Gump/FEcontrolgump.cs
@@ -0,0 +1,120 @@
+using System;
+using System.Collections;
+using Server;
+using Server.Network;
+using Server.Mobiles;
+using Server.Items;
+using Server.Misc;
+using System.Collections.Generic;
+using Server.Accounting;
+using System.Linq;
+
+
+namespace Server.Gumps
+{
+    public class FeControlGump : BaseProjectMGump
+	{
+   
+        private CustomPlayerMobile m_From;
+		private int m_Page;
+		Dictionary<CustomPlayerMobile, int> Dictionnairy;
+
+		public FeControlGump(CustomPlayerMobile from,  Dictionary<CustomPlayerMobile, int> dict, int page = 0)
+            : base("Fe", 560, 622, true)
+        {
+
+			m_From = from;
+			m_Page = page;
+
+			int x = XBase;
+			int y = YBase;
+
+	
+			int line = 0;
+			
+			Dictionnairy = dict;
+
+
+
+			if (Dictionnairy.Count == 0)
+			{
+
+				Dictionary<CustomPlayerMobile, int> mydict = new Dictionary<CustomPlayerMobile, int>();
+
+				foreach (Account acct in Accounts.GetAccounts())
+				{
+					for (int i = 0; i < acct.Length; i++)
+					{
+						if (acct[i] != null)
+						{
+							if (acct[i] is CustomPlayerMobile cp)
+							{
+								mydict.Add(cp, cp.FETotal);
+							}
+
+							
+						}
+			
+					}
+				}
+
+				Dictionnairy =  (from entry in mydict orderby entry.Value descending select entry).ToDictionary(pair => pair.Key, pair => pair.Value); ;
+			}
+
+
+			int i2 = 0;
+
+			AddHtmlTexteColored(x + 10, y + 20 + line * 20, 300, "Personnage", "#ffffff");
+			AddHtmlTexteColored(x + 300, y + 20 + line * 20, 300, "Total FE", "#ffffff");
+			AddHtmlTexteColored(x + 400, y + 20 + line * 20, 300, "FE Normal", "#ffffff");
+			AddHtmlTexteColored(x + 500, y + 20 + line * 20, 300, "FE RP", "#ffffff");
+
+
+			foreach (KeyValuePair<CustomPlayerMobile, int> item in Dictionnairy)
+			{
+				if (i2 >= page * 28 && line < 28)
+				{
+					AddHtmlTexteColored(x + 10 , y + 40 + line * 20, 300, item.Key.Name, "#ffffff");
+					AddHtmlTexteColored(x + 300, y + 40 + line * 20, 300, item.Value.ToString(), "#ffffff");
+					AddHtmlTexteColored(x + 400, y + 40 + line * 20, 300, item.Key.FENormalTotal.ToString(), "#ffffff");
+					AddHtmlTexteColored(x + 500, y + 40 + line * 20, 300, item.Key.FERPTotal.ToString(), "#ffffff");
+					line++;
+				}
+				i2++;
+			}
+
+			if (page != 0)
+			{
+				AddButton(x + 5, y + 610, 1, 4506);
+			}
+			if (Server.World.Mobiles.Count > (page + 1) * 28)
+			{
+				AddButton(x + 535, y + 610, 2, 4502);
+			}
+
+
+
+		}
+
+		public override void OnResponse(NetState sender, RelayInfo info)
+        {
+
+
+
+			     switch (info.ButtonID)
+				 {
+					 case 1:
+						 {
+							sender.Mobile.SendGump(new FeControlGump(m_From, Dictionnairy, m_Page - 1));
+							 break;
+						 }
+					 case 2:
+						 {
+							sender.Mobile.SendGump(new FeControlGump(m_From, Dictionnairy, m_Page + 1));
+							break;
+						 }
+
+				 }
+		}
+	}
+}

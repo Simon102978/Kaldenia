7a42749695ae777650df6478eee7876c626c0e5d
Nucca411 <105027759+Nucca411@users.noreply.github.com>
2022-07-10 20:49:23 -0400

    witchcraft, prix de larene, crabfisher

--- Scripts/VendorInfo/SBCrabFisher.cs
+++ Scripts/VendorInfo/SBCrabFisher.cs
@@ -1,4 +1,4 @@
-using Server.Items;
+﻿using Server.Items;
 using System.Collections.Generic;
 
 namespace Server.Mobiles
@@ -23,35 +23,69 @@ namespace Server.Mobiles
         {
             public InternalSellInfo()
             {
-                Add(typeof(LobsterTrap), 10);
+				Add(typeof(RawFishSteak), 2);
+				Add(typeof(Fish), 3);
+				Add(typeof(SmallFish), 1);
+				Add(typeof(FishingPole), 7);
+				Add(typeof(LobsterTrap), 10);
 				Add(typeof(Crab), 3);
 				Add(typeof(AppleCrab), 3);
-                Add(typeof(BlueCrab), 5);
-                Add(typeof(DungeonessCrab), 7);
-                Add(typeof(KingCrab), 10);
-                Add(typeof(RockCrab), 15);
-                Add(typeof(SnowCrab), 20);
-                Add(typeof(StoneCrab), 25);
-                Add(typeof(SpiderCrab), 25);
-                Add(typeof(TunnelCrab), 25);
-                Add(typeof(VoidCrab), 25);
+				Add(typeof(BlueCrab), 5);
+				Add(typeof(DungeonessCrab), 7);
+				Add(typeof(KingCrab), 9);
+				Add(typeof(RockCrab), 10);
+				Add(typeof(SnowCrab), 10);
+				Add(typeof(StoneCrab), 10);
+				Add(typeof(SpiderCrab), 10);
+				Add(typeof(TunnelCrab), 10);
+				Add(typeof(VoidCrab), 10);
 
 				Add(typeof(Lobster), 3);
 				Add(typeof(CrustyLobster), 3);
-                Add(typeof(FredLobster), 5);
-                Add(typeof(HummerLobster), 7);
-                Add(typeof(RockLobster), 10);
-                Add(typeof(ShovelNoseLobster), 15);
-                Add(typeof(SpineyLobster), 20);
-                Add(typeof(BlueLobster), 25);
-                Add(typeof(BloodLobster), 25);
-                Add(typeof(DreadLobster), 25);
-                Add(typeof(VoidLobster), 25);
+				Add(typeof(FredLobster), 5);
+				Add(typeof(HummerLobster), 7);
+				Add(typeof(RockLobster), 8);
+				Add(typeof(ShovelNoseLobster), 9);
+				Add(typeof(SpineyLobster), 10);
+				Add(typeof(BlueLobster), 10);
+				Add(typeof(BloodLobster), 10);
+				Add(typeof(DreadLobster), 10);
+				Add(typeof(VoidLobster), 10);
 
-                Add(typeof(StoneCrabMeat), 1);
-                Add(typeof(SpiderCrabMeat), 1);
-                Add(typeof(BlueLobsterMeat), 1);
-            }
+				Add(typeof(StoneCrabMeat), 5);
+				Add(typeof(SpiderCrabMeat), 5);
+				Add(typeof(BlueLobsterMeat), 5);
+
+				/// Ajouts poissons speciaux ///
+
+				Add(typeof(AutumnDragonfish), 5);
+				Add(typeof(BullFish), 3);
+				Add(typeof(CrystalFish), 3);
+				Add(typeof(FairySalmon), 3);
+				Add(typeof(FireFish), 3);
+				Add(typeof(GiantKoi), 4);
+				Add(typeof(GreatBarracuda), 4);
+				Add(typeof(HolyMackerel), 4);
+				Add(typeof(LavaFish), 4);
+				Add(typeof(ReaperFish), 4);
+				Add(typeof(SummerDragonfish), 5);
+				Add(typeof(UnicornFish), 5);
+				Add(typeof(YellowtailBarracuda), 5);
+				Add(typeof(AbyssalDragonfish), 6);
+				Add(typeof(BlackMarlin), 6);
+				Add(typeof(BlueMarlin), 6);
+				Add(typeof(DungeonPike), 6);
+				Add(typeof(GiantSamuraiFish), 6);
+				Add(typeof(GoldenTuna), 6);
+				Add(typeof(Kingfish), 6);
+				Add(typeof(LanternFish), 6);
+				Add(typeof(RainbowFish), 6);
+				Add(typeof(SeekerFish), 6);
+				Add(typeof(SpringDragonfish), 6);
+				Add(typeof(StoneFish), 6);
+				Add(typeof(WinterDragonfish), 6);
+				Add(typeof(ZombieFish), 6);
+			}
         }
     }
 }

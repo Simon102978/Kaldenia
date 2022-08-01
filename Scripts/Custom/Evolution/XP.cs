525af853ec057d180d30ceebc23ae34512448d33
Simon <mitaniel@hotmail.com>
2022-08-01 15:47:20 -0400

    retour des modifications au système d'Experience (Fe en attente et fix au 30 minutes)

--- Scripts/Custom/Evolution/XP.cs
+++ Scripts/Custom/Evolution/XP.cs
@@ -45,33 +45,38 @@ namespace Server
         {
           Mobile m = state.Mobile;
 
-          if (m != null && m is CustomPlayerMobile)
-          {
-			CustomPlayerMobile pm = (CustomPlayerMobile)m;
-			
-			if(pm.FENormalTotal >= day * 5 )
-			{
+          if (m != null && m is CustomPlayerMobile pm && !pm.Jail)
+		  {
 
-			}
-            else if (pm.NextFETime < TimeSpan.FromMinutes(10))
-            {
-             // if (pm.DailyFECount < 4)
-             // {
-                GainFE(pm);
-                ResetFETime(pm);
-            //  }
-            }
-            else
-            {
-              if (pm.LastLoginTime < DateTime.Now - TimeSpan.FromMinutes(10))
-              {
-                pm.NextFETime -= TimeSpan.FromMinutes(10);
-              }
-              else
-              {
-                pm.NextFETime -= DateTime.Now - pm.LastLoginTime;
-              }
-            }
+			if (pm.NextFETime <= TimeSpan.FromMinutes(10))
+			{
+				if (pm.FENormalTotal >= day * 5)
+				{
+					if (pm.FEAttente > 0)
+					{
+						pm.FEAttente--;
+						pm.FE++;
+						pm.SendMessage("Vous récupérer une FE en attente !");
+					}
+				}
+				else
+	    		{
+					GainFE(pm);
+				}
+
+				ResetFETime(pm);
+			}		  
+		    else
+		    {
+				  if (pm.LastLoginTime < DateTime.Now - TimeSpan.FromMinutes(10))
+				  {
+					pm.NextFETime -= TimeSpan.FromMinutes(10);
+				  }
+				  else
+				  {
+					pm.NextFETime -= DateTime.Now - pm.LastLoginTime;
+				  }
+		    }
           }
         }
       }
@@ -130,7 +135,7 @@ namespace Server
       pm.FE++;
 	  pm.FENormalTotal++;
 
-	 pm.SendMessage("Vous obtenez une nouvelle FE !");
+	  pm.SendMessage("Vous obtenez une nouvelle FE !");
 
 			if (pm.FEAttente > 0)
 			{
@@ -155,206 +160,206 @@ namespace Server
 			}
     
     }
+  }
+	/*  public static void SetSkills(CustomPlayerMobile from, int skillcaptotal, double skillcapind)
+	  {
+		from.SkillsCap = skillcaptotal;
 
-  /*  public static void SetSkills(CustomPlayerMobile from, int skillcaptotal, double skillcapind)
-    {
-      from.SkillsCap = skillcaptotal;
+		for (int i = 0; i < from.Skills.Length; ++i)
+		{
+		  //if (!IsLoreSkill(from.Skills[i]))
+		  from.Skills[i].Cap = (double)skillcapind;
+		}
+	  }
+	*/
+	/*   public static void SetPAs(CustomPlayerMobile from)
+	   {
+		 /*from.AptitudesLibres++;
 
-      for (int i = 0; i < from.Skills.Length; ++i)
-      {
-        //if (!IsLoreSkill(from.Skills[i]))
-        from.Skills[i].Cap = (double)skillcapind;
-      }
-    }
-  */
- /*   public static void SetPAs(CustomPlayerMobile from)
-    {
-      /*from.AptitudesLibres++;
+		 int paEnAttente = Aptitudes.GetRemainingPA(from) - Aptitudes.GetDisponiblePA(from);
 
-      int paEnAttente = Aptitudes.GetRemainingPA(from) - Aptitudes.GetDisponiblePA(from);
+		 //if (paEnAttente > 15)
+		 //    paEnAttente = 15;
 
-      //if (paEnAttente > 15)
-      //    paEnAttente = 15;
+		 from.AptitudesLibres += paEnAttente;*/
+	//   }
 
-      from.AptitudesLibres += paEnAttente;*/
-    }
+	/*   public static int GetNeededFE(CustomPlayerMobile pm)
+	   {
+		 if (pm == null)
+		   return 0;
 
- /*   public static int GetNeededFE(CustomPlayerMobile pm)
-    {
-      if (pm == null)
-        return 0;
+		 int neededFE = 0;
 
-      int neededFE = 0;
+		 if (pm.Niveau > 30)
+		 {
+		   neededFE = 210 + (12 * (pm.Niveau - 30));
+		 }
+		 else if (pm.Niveau > 40)
+		 {
+		   neededFE = 340 + (20 * (pm.Niveau - 40));
+		 }
+		 else
+		 {
+		   neededFE = m_FeReqTable[pm.Niveau - 1];
+		 }
 
-      if (pm.Niveau > 30)
-      {
-        neededFE = 210 + (12 * (pm.Niveau - 30));
-      }
-      else if (pm.Niveau > 40)
-      {
-        neededFE = 340 + (20 * (pm.Niveau - 40));
-      }
-      else
-      {
-        neededFE = m_FeReqTable[pm.Niveau - 1];
-      }
+		 return neededFE;
+	   }*/
 
-      return neededFE;
-    }*/
+	/*  public static bool CanEvolve(Mobile from)
+	  {
+		try
+		{
+		  if (from is CustomPlayerMobile)
+		  {
+			CustomPlayerMobile pm = from as CustomPlayerMobile;
 
-  /*  public static bool CanEvolve(Mobile from)
-    {
-      try
-      {
-        if (from is CustomPlayerMobile)
-        {
-		  CustomPlayerMobile pm = from as CustomPlayerMobile;
+			int currentXP = pm.FE;
+			int neededXP = GetNeededFE(pm);
 
-          int currentXP = pm.FE;
-          int neededXP = GetNeededFE(pm);
+			if (currentXP > neededXP)
+			{
+			  return true;
+			}
+		  }
+		}
+		catch (Exception ex)
+		{
+		  Console.WriteLine(ex.ToString());
+		}
 
-          if (currentXP > neededXP)
-          {
-            return true;
-          }
-        }
-      }
-      catch (Exception ex)
-      {
-        Console.WriteLine(ex.ToString());
-      }
+		return false;
+	  }
 
-      return false;
-    }
+	  public static void Evolve(Mobile from)
+	  {
+		try
+		{
+		  if (from is CustomPlayerMobile)
+		  {
+		   CustomPlayerMobile pm = from as CustomPlayerMobile;
 
-    public static void Evolve(Mobile from)
-    {
-      try
-      {
-        if (from is CustomPlayerMobile)
-        {
-	     CustomPlayerMobile pm = from as CustomPlayerMobile;
+			int currentFE = pm.FE;
+			int neededFE = GetNeededFE(pm);
 
-          int currentFE = pm.FE;
-          int neededFE = GetNeededFE(pm);
+			if (currentFE > neededFE)
+			{
+			  pm.Niveau++;
 
-          if (currentFE > neededFE)
-          {
-            pm.Niveau++;
+			  int SkillsCaps = 100;
+			  if (pm.Niveau > 0 && pm.Niveau < 31)
+			  {
+				SkillsCaps = m_SkillCapTotalTable[pm.Niveau - 1];
+			  }
+			  else if (pm.Niveau > 30)
+			  {
+				SkillsCaps = 800 + ((pm.Niveau - 30) * 10);
+			  }
+			  else if (pm.Niveau > 40)
+			  {
+				SkillsCaps = 900 + ((pm.Niveau - 40) * 5);
+			  }
 
-            int SkillsCaps = 100;
-            if (pm.Niveau > 0 && pm.Niveau < 31)
-            {
-              SkillsCaps = m_SkillCapTotalTable[pm.Niveau - 1];
-            }
-            else if (pm.Niveau > 30)
-            {
-              SkillsCaps = 800 + ((pm.Niveau - 30) * 10);
-            }
-            else if (pm.Niveau > 40)
-            {
-              SkillsCaps = 900 + ((pm.Niveau - 40) * 5);
-            }
+			  double SkillsInd = 45;
+			  if (pm.Niveau > 0 && pm.Niveau < 31)
+			  {
+				SkillsInd = m_SkillCapIndividuelTable[pm.Niveau - 1];
+			  }
+			  else if (pm.Niveau > 30)
+			  {
+				SkillsInd = 100;
+			  }
 
-            double SkillsInd = 45;
-            if (pm.Niveau > 0 && pm.Niveau < 31)
-            {
-              SkillsInd = m_SkillCapIndividuelTable[pm.Niveau - 1];
-            }
-            else if (pm.Niveau > 30)
-            {
-              SkillsInd = 100;
-            }
+			  if (SkillsInd > 100)
+				SkillsInd = 100;
 
-            if (SkillsInd > 100)
-              SkillsInd = 100;
+			  /*if (SkillsCaps > 800)
+				SkillsCaps = 800;*/
+	/*
+			  SetSkills(pm, SkillsCaps, SkillsInd);
+			  SetPAs(pm);
 
-            /*if (SkillsCaps > 800)
-              SkillsCaps = 800;*/
-  /*
-            SetSkills(pm, SkillsCaps, SkillsInd);
-            SetPAs(pm);
+			  pm.SendMessage("Vous gagnez un niveau !");
+			}
+			else
+			  pm.SendMessage("Il vous manque des points d'experiences pour gagner votre niveau !");
+		  }
+		}
+		catch (Exception ex)
+		{
+		  Console.WriteLine(ex.ToString());
+		}
+	  }*/
+	//  }
+	/*  public class Cote
+	  {
+		public void Serialize(GenericWriter writer)
+		{
+		  writer.Write((int)0); // version;
 
-            pm.SendMessage("Vous gagnez un niveau !");
-          }
-          else
-            pm.SendMessage("Il vous manque des points d'experiences pour gagner votre niveau !");
-        }
-      }
-      catch (Exception ex)
-      {
-        Console.WriteLine(ex.ToString());
-      }
-    }*/
-//  }
-/*  public class Cote
-  {
-    public void Serialize(GenericWriter writer)
-    {
-      writer.Write((int)0); // version;
+		  writer.Write((Mobile)m_coteur);
+		  writer.Write((DateTime)m_cotetime);
+		  writer.Write((int)m_rating);
+		}
 
-      writer.Write((Mobile)m_coteur);
-      writer.Write((DateTime)m_cotetime);
-      writer.Write((int)m_rating);
-    }
+		public Cote(CustomPlayerMobile owner, GenericReader reader)
+		{
+		  int version = reader.ReadInt();
 
-    public Cote(CustomPlayerMobile owner, GenericReader reader)
-    {
-      int version = reader.ReadInt();
+		  m_coteur = reader.ReadMobile();
+		  m_cotetime = reader.ReadDateTime();
+		  m_rating = reader.ReadInt();
+		}
 
-      m_coteur = reader.ReadMobile();
-      m_cotetime = reader.ReadDateTime();
-      m_rating = reader.ReadInt();
-    }
+		private Mobile m_coteur;
+		private DateTime m_cotetime;
+		private int m_rating;
 
-    private Mobile m_coteur;
-    private DateTime m_cotetime;
-    private int m_rating;
+		[CommandProperty(AccessLevel.GameMaster)]
+		public DateTime CoteDate
+		{
+		  get { return m_cotetime; }
+		  set { m_cotetime = value; }
+		}
+		[CommandProperty(AccessLevel.GameMaster)]
+		public Mobile Coteur
+		{
+		  get { return m_coteur; }
+		  set { m_coteur = value; }
+		}
+		[CommandProperty(AccessLevel.GameMaster)]
+		public int Rating
+		{
+		  get { return m_rating; }
+		  set { m_rating = value; }
+		}
 
-    [CommandProperty(AccessLevel.GameMaster)]
-    public DateTime CoteDate
-    {
-      get { return m_cotetime; }
-      set { m_cotetime = value; }
-    }
-    [CommandProperty(AccessLevel.GameMaster)]
-    public Mobile Coteur
-    {
-      get { return m_coteur; }
-      set { m_coteur = value; }
-    }
-    [CommandProperty(AccessLevel.GameMaster)]
-    public int Rating
-    {
-      get { return m_rating; }
-      set { m_rating = value; }
-    }
+		public Cote()
+		{
+		}
 
-    public Cote()
-    {
-    }
+		public Cote(Mobile Coteur)
+		{
+		  m_coteur = Coteur;
+		}
 
-    public Cote(Mobile Coteur)
-    {
-      m_coteur = Coteur;
-    }
+		public Cote(Mobile Coteur, DateTime date)
+		{
+		  m_cotetime = date;
+		  m_coteur = Coteur;
+		}
 
-    public Cote(Mobile Coteur, DateTime date)
-    {
-      m_cotetime = date;
-      m_coteur = Coteur;
-    }
+		public Cote(Mobile Coteur, DateTime date, int rating)
+		{
+		  m_cotetime = CoteDate;
+		  m_coteur = Coteur;
+		  m_rating = Rating;
+		}
 
-    public Cote(Mobile Coteur, DateTime date, int rating)
-    {
-      m_cotetime = CoteDate;
-      m_coteur = Coteur;
-      m_rating = Rating;
-    }
-
-    public int GetCoteRating()
-    {
-      return m_rating;
-    }
-  }*/
+		public int GetCoteRating()
+		{
+		  return m_rating;
+		}
+	  }*/
 }

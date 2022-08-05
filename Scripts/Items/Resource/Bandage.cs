3e2e015047a0b9af11e25ff262152e6566c4c60b
Simon <mitaniel@hotmail.com>
2022-08-05 00:23:44 -0400

    Correction des bandages et des morts

--- Scripts/Items/Resource/Bandage.cs
+++ Scripts/Items/Resource/Bandage.cs
@@ -711,7 +711,7 @@ namespace Server.Items
             {
                 healer.SendLocalizedMessage(501042); // Target cannot be resurrected at that location.
             }
-			else if (patient is CustomPlayerMobile cp && cp.Vulnerability)
+			else if (patient is CustomPlayerMobile cp && cp.Vulnerability && cp.Alive)
 			{
 				healer.SendMessage("Vous ne pouvez pas soigner une cible qui vient d'être assomer avec des bandages.");
 			}

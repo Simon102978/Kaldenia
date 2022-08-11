4c3b6566cfb45a65f1c2c3b8f7fdc5669bce22b5
Simon <mitaniel@hotmail.com>
2022-08-11 17:30:23 -0400

    Les titles sont maintenant afficher sur les mobs. Ogrelord retour a 1 tiles

--- Scripts/Mobiles/Normal/OgreLord.cs
+++ Scripts/Mobiles/Normal/OgreLord.cs
@@ -6,7 +6,7 @@ namespace Server.Mobiles
 	{
         [Constructable]
         public OgreLord()
-            : base(AIType.AI_Melee, FightMode.Closest, 10, 2, 0.2, 0.4)
+            : base(AIType.AI_Melee, FightMode.Closest, 10,1, 0.2, 0.4)
         {
             Name = "Seigneur Ogre";
             Body = 83;

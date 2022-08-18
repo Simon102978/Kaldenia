45a33c9f2beeafe863d6d62a72c1bac09818b951
Nucca411 <105027759+Nucca411@users.noreply.github.com>
2022-08-17 22:06:38 -0400

    agressor

--- Scripts/Mobiles/Summons/EnergyVortex.cs
+++ Scripts/Mobiles/Summons/EnergyVortex.cs
@@ -13,7 +13,7 @@ namespace Server.Mobiles
 
         [Constructable]
         public EnergyVortex(bool summoned)
-            : base(AIType.AI_Melee, FightMode.Closest, 10, 1, 0.2, 0.4)
+            : base(AIType.AI_Melee, FightMode.Aggressor, 10, 1, 0.2, 0.4)
         {
             Name = "an energy vortex";
 
